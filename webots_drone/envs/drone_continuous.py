fix orientation and reward calculation

webots_drone/envs/drone_continuous.py
@@ -290,12 +290,12 @@ class DroneEnvContinuous(gym.Env):
         uav_xy = info['position'][:2]
         target_xy = self.sim.get_target_pos()[:2]
         # orientation values from [-1, 1] to [0, 2 * pi]
-        uav_ori = (info['north_deg'] + 1.) * np.pi
+        uav_ori = info['north_deg']
         # compute reward components
         orientation_reward = compute_orientation_reward(uav_xy, uav_ori,
                                                         target_xy)
         distance_reward = compute_distance_reward(
-            uav_xy, target_xy, distance_max=25.,
+            uav_xy, target_xy, distance_max=50.,
             distance_threshold=np.sum(self._goal_threshold),
             threshold_offset=self._goal_threshold[1])
         reward = sum_rewards(orientation_reward, distance_reward)

webots_drone/reward.py
@@ -8,7 +8,6 @@ Created on Wed Nov 22 11:35:08 2023
 
 import numpy as np
 from webots_drone.utils import compute_distance
-from webots_drone.utils import min_max_norm
 
 
 def compute_direction_vector(position, orientation, ref_position):
@@ -36,9 +35,9 @@ def compute_orientation_reward(position, orientation, ref_position):
     return cosine_similarity
 
 
-def compute_distance_reward(position, ref_position, distance_max=25.,
-                            distance_threshold=5.,
-                            threshold_offset=2.):
+def compute_distance_reward(position, ref_position, distance_max=50.,
+                            distance_threshold=25.,
+                            threshold_offset=5.):
     curr_distance = compute_distance(position, ref_position)
 
     if curr_distance < distance_threshold - threshold_offset:
@@ -91,6 +90,12 @@ if __name__ == '__main__':
         plt.xlabel('Position X')
         plt.ylabel('Position Y')
         plt.title('Reward Heatmap considering Position (X, Y) and Orientation')
+        # Calculate vector components
+        headings_rad = np.linspace(0, 2 * np.pi, num=100)
+        dx = np.cos(headings_rad)
+        dy = np.sin(headings_rad)
+        plt.quiver(x_grid[1], y_grid[1], dx, dy, angles='xy', scale_units='xy', scale=1)
+        plt.grid()
         plt.show()
 
     # Define the range of positions and orientations

webots_drone/webots_simulation.py
@@ -17,7 +17,7 @@ from webots_drone.utils import emitter_send_json
 from webots_drone.utils import decode_image
 from webots_drone.utils import min_max_norm
 from webots_drone.utils import compute_distance
-from webots_drone.utils import compute_flight_area
+from webots_drone.utils import preprocess_orientation
 
 
 # Webots environment controller
@@ -299,6 +299,8 @@ class WebotsSimulation(Supervisor):
         orientation[2] = min_max_norm(orientation[2],  # yaw
                                       a=-1, b=1,
                                       minx=-np.pi, maxx=np.pi)
+        # Apply offset and preprocess orientation
+        north_deg = preprocess_orientation(north_deg)
 
         # Normalize distance sensor values
         for idx, sensor in uav_state['dist_sensors'].items():
@@ -308,11 +310,6 @@ class WebotsSimulation(Supervisor):
                                  a=0, b=1,
                                  minx=sensor[1], maxx=sensor[2])
             dist_sensors.append(s_val)
-
-        # Normalize north degree
-        north_deg = min_max_norm(north_deg,
-                                  a=-1, b=1,
-                                  minx=0, maxx=360)
 
         if type(uav_state['image']) == str and uav_state['image'] == "NoImage":
             img = np.zeros(self.image_shape)